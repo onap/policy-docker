1766552905dd91720f2eeabc47269a9478ac8195
Pamela Dragosh <pdragosh@research.att.com>
2017-02-21 09:05:08 -0500

    moving docker build to local
    Change-Id: Id39721049742404842e63cd56989143210059150
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

---
+++ docker_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+echo '============== STARTING SCRIPT TO BUILD DOCKER IMAGES ================='
+
+
+DOCKER_REPOSITORY=nexus3.openecomp.org:10003
+DOCKER_VERSION=latest
+
+for image in policy-os policy-nexus policy-db policy-base policy-drools policy-pe ; do
+    echo "Building $image"
+    mkdir -p target/$image
+    cp $image/* target/$image
+    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy/$image:${DOCKER_VERSION} target/$image
+    docker images
+done
+
+for image in policy-nexus policy-db policy-drools policy-pe; do
+    echo "Pushing $image"
+    docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image:${DOCKER_VERSION}
+done
