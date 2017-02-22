4c8a4135774d640d4083820231c6ac3e81c8e6d0
Pamela Dragosh <pdragosh@research.att.com>
2017-02-22 14:40:47 -0500

    hardcoding nexus for demo team
    Change-Id: I69f516128a3209c40441de568bac506b09f4ea45
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- docker_verify.sh
+++ docker_verify.sh
@@ -6,13 +6,3 @@ echo '============== STARTING SCRIPT TO BUILD DOCKER IMAGES ================='
 DOCKER_REPOSITORY=nexus3.openecomp.org:10003
 DOCKER_VERSION=latest
 
-cp policy-pe/* target/policy-pe/
-cp policy-drools/* target/policy-drools/
-
-for image in policy-os policy-nexus policy-db policy-base policy-drools policy-pe ; do
-    echo "Building $image"
-    mkdir -p target/$image
-    cp $image/* target/$image
-    docker build --quiet --tag openecomp/policy/$image:${DOCKER_VERSION} --tag ${DOCKER_REPOSITORY}/openecomp/policy/$image:${DOCKER_VERSION} target/$image
-    docker images
-done
