6c324b6883377b17b8be3839b9192bed572f6010
Pamela Dragosh <pdragosh@research.att.com>
2017-03-23 07:44:56 -0400

    changing to NEXUS_DOCKER_REPO via Marco. Tags
    Change-Id: I8cdbf98141070f89dd3c668a1aa3317fc9df2683
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- docker_build.sh
+++ docker_build.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 #
 echo '============== STARTING SCRIPT TO BUILD DOCKER IMAGES ================='
-
-#
-# THIS SHOULD BE REPLACED WITH SPECIFIC REPOSITORY FOR THIS RELEASE
-#
 DOCKER_REPOSITORY=nexus3.openecomp.org:10003
-#
-# RELEASE BUILD CREATES latest tag and the STAGING tag
-#
 MVN_VERSION=$(cat target/version)
-MVN_VERSION="${MVN_VERSION}-STAGING"
+MVN_MAJMIN_VERSION=$(cut -d . -f1 -f2 target/version)
 TIMESTAMP=$(date -u +%Y%m%dT%H%M%S)
 
 echo $DOCKER_REPOSITORY
 echo $MVN_VERSION
+echo $MVN_MAJMIN_VERSION
 echo $TIMESTAMP
+
+if [[ -z $MVN_VERSION ]]
+then
+    echo "MVN_VERSION is empty"
+    exit 1
+fi
+
+if [[ -z $MVN_MAJMIN_VERSION ]]
+then
+    echo "MVN_MAJMIN_VERSION is empty"
+    exit 1
+fi
+
+if [[ $MVN_VERSION == *"SNAPSHOT"* ]]
+then
+    MVN_MAJMIN_VERSION="${MVN_MAJMIN_VERSION}-SNAPSHOT"
+else
+    MVN_MAJMIN_VERSION="${MVN_MAJMIN_VERSION}-STAGING"
+fi
+
+echo $MVN_MAJMIN_VERSION
 
 cp policy-pe/* target/policy-pe/
 cp policy-drools/* target/policy-drools/
@@ -25,20 +40,27 @@ for image in policy-os policy-nexus policy-db policy-base policy-drools policy-p
     mkdir -p target/$image
     cp $image/* target/$image
 
+    #
+    # This is the local latest tagged image. The Dockerfile's need this to build images
+    #
     TAGS="--tag openecomp/policy/${image}:latest"
-    TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/openecomp/policy/${image}:latest"
-    TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/openecomp/policy/${image}:${MVN_VERSION}-latest"
-    TAGS="${TAGS} --tag openecomp/policy/${image}:${MVN_VERSION}-${TIMESTAMP}"
+    #
+    # This has the nexus repo prepended and only major/minor version with latest
+    #
+    TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/openecomp/policy/${image}:${MVN_MAJMIN_VERSION}-latest"
+    #
+    # This has the nexus repo prepended and major/minor/patch version with timestamp
+    #
     TAGS="${TAGS} --tag ${DOCKER_REPOSITORY}/openecomp/policy/${image}:${MVN_VERSION}-${TIMESTAMP}"
 
     echo $TAGS
 
     docker build --quiet $TAGS target/$image
-    docker images
 done
 
 for image in policy-nexus policy-db policy-drools policy-pe; do
     echo "Pushing $image"
     docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image:latest
+    docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image:${MVN_MAJMIN_VERSION}-latest
     docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image:${MVN_VERSION}-${TIMESTAMP}
 done
