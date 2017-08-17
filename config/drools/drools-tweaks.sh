cc55d777d2f100479a6eb2d1c80a30f97dc62848
Jorge Hernandez <jh1730@att.com>
2017-08-17 15:16:53 -0500

    remove DCAE custom config from drools-tweaks.sh
    It has become unnessary since drools-tweaks since the drools-applications
    side is changing to use configurable values from base.conf
    
    Change-Id: I1a601b27a3acbd3b4d90e5a023c2f2fe8b0861d0
    Issue-ID: POLICY-159
    Signed-off-by: Jorge Hernandez <jh1730@att.com>

--- config/drools/drools-tweaks.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-${POLICY_HOME}/bin/features enable healthcheck
-
-sed -i -e 's/DCAE-CL-EVENT/unauthenticated.TCA_EVENT_OUTPUT/' \
-       -e '/TCA_EVENT_OUTPUT\.servers/s/servers=.*$/servers=10.0.4.102/' \
-    $POLICY_HOME/config/v*-controller.properties
