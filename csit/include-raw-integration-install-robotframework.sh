8fa82fdedd8c1bd723f0c83a61f7703bf6a2b696
arkadiusz.adamski <aadamski@est.tech>
2021-05-24 12:50:40 +0100

    Use python3 for CSITs
    - use python3 and pip3 in scripts
    - update robot-framework-http library, so it can use python3
    - update selenium2library, so it can use python3
    
    Issue-ID: POLICY-3092
    Signed-off-by: arkadiusz.adamski <aadamski@est.tech>
    Change-Id: I5a6a4fe43e54366859049e1d43bbce599dacd14e

---
+++ csit/include-raw-integration-install-robotframework.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -l
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2015 The Linux Foundation and others.
+# Modification Copyright 2021. Nordix Foundation.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+# vim: sw=4 ts=4 sts=4 et ft=sh :
+
+ROBOT_VENV="/tmp/v/robot"
+echo ROBOT_VENV="${ROBOT_VENV}" >> "${WORKSPACE}/env.properties"
+
+# The --system-site-packages parameter allows us to pick up system level
+# installed packages. This allows us to bake matplotlib which takes very long
+# to install into the image.
+python3 -m venv "${ROBOT_VENV}"
+
+source "${ROBOT_VENV}/bin/activate"
+
+set -exu
+
+# Make sure pip3 itself us up-to-date.
+python3 -m pip3 install --upgrade pip3
+
+echo "Installing Python Requirements"
+cat << 'EOF' > "requirements.txt"
+docker-py
+ipaddr
+netaddr
+netifaces
+pyhocon
+requests
+robotframework-httplibrary
+robotframework-requests
+robotframework-selenium2library
+robotframework-extendedselenium2library
+robotframework-sshlibrary
+scapy
+# Module jsonpath is needed by current AAA idmlite suite.
+jsonpath-rw
+# Modules for longevity framework robot library
+elasticsearch
+elasticsearch-dsl
+# Module for pyangbind used by lispflowmapping project
+pyangbind
+# Module for iso8601 datetime format
+isodate
+# Module for TemplatedRequests.robot library
+jmespath
+# Module for backup-restore support library
+jsonpatch
+# odltools for extra debugging
+odltools
+EOF
+python3 -m pip3 install -r requirements.txt
+odltools -V
+pip3 freeze
