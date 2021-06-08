c547508e5721e0054494e228a77d1b649a0687f6
Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
2021-06-08 09:28:21 +0100

    Add CSIT for clamp controlloop
    Add CSIT testcases, scripts and a seperate docker-compose file for
    integration testing between controlloop components, this involves
    integration between Mariadb, Dmaap, ControlLoop-Runtime,
    Policy-Participant, Policy-API and DCAE-Participant docker images.
    
    Issue-ID: POLICY-3217
    Signed-off-by: Sirisha_Manchikanti <sirisha.manchikanti@est.tech>
    Change-Id: Id7cb1451afc4331b4ed1ad1ac6c2528a747eaaa0

---
+++ csit/clamp/plans/detmVers.sh
@@ -0,0 +1,42 @@
+# ============LICENSE_START====================================================
+#  Copyright (C) 2020-2021 AT&T Intellectual Property. All rights reserved.
+# =============================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END======================================================
+
+source ${SCRIPTS}/get-branch-mariadb.sh
+
+echo POLICY_MARIADB_VER=${POLICY_MARIADB_VER}
+
+function getVersion
+{
+    REPO=$1
+    curl -qL --silent \
+      https://github.com/onap/policy-$REPO/raw/${GERRIT_BRANCH}/pom.xml |
+    xmllint --xpath \
+      '/*[local-name()="project"]/*[local-name()="version"]/text()' -
+}
+
+POLICY_MODELS_VERSION=$(getVersion models)
+export POLICY_MODELS_VERSION=${POLICY_MODELS_VERSION:0:3}-SNAPSHOT-latest
+echo POLICY_MODELS_VERSION=${POLICY_MODELS_VERSION}
+
+POLICY_API_VERSION=$(getVersion api)
+export POLICY_API_VERSION=${POLICY_API_VERSION:0:3}-SNAPSHOT-latest
+echo POLICY_API_VERSION=${POLICY_API_VERSION}
+
+POLICY_CLAMP_VERSION=$(getVersion clamp)
+export POLICY_CLAMP_VERSION=${POLICY_CLAMP_VERSION:0:5}-SNAPSHOT-latest
+echo POLICY_CLAMP_VERSION=${POLICY_CLAMP_VERSION}
