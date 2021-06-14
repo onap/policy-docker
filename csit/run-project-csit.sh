548aa5aaf6e0c5fbcc92430855ddb24584352bd2
Jim Hahn <jrh3@att.com>
2021-06-14 12:00:48 -0400

    Don't save private key
    GitHub complains if you include a private key in a repo.  Modified the
    CSITs to generate the root CA so that it's private key does not have to
    be stored.
    
    Issue-ID: POLICY-3384
    Change-Id: I4bebc3e4b0e386047d7f6fbd19150812cb605899
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- csit/run-project-csit.sh
+++ csit/run-project-csit.sh
@@ -167,7 +167,8 @@ cd "${WORKDIR}"
 # Sign in to nexus3 docker repo
 docker login -u docker -p docker nexus3.onap.org:10001
 
-# Generate keystore to be used by repos
+# Generate truststore and keystore to be used by repos
+${SCRIPTS}/gen_truststore.sh
 ${SCRIPTS}/gen_keystore.sh
 cp ${SCRIPTS}/config/ks.jks ${SCRIPTS}/config/drools/custom/policy-keystore
 cp ${SCRIPTS}/config/ks.jks ${SCRIPTS}/config/drools-apps/custom/policy-keystore
