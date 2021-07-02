edf09e8264bdeefdcb20d566feee0b54005b775b
ktimoney <kevin.timoney@est.tech>
2021-07-02 15:19:47 +0100

    Move drools-pdp db-migrator to separate container
    Note: Instructions on how to use: https://wiki.onap.org/display/DW/Instructions+for+running+db-migrator
    
    Issue-ID: POLICY-3458
    Change-Id: I42bce63eca94333c6aaab0e27eb3e514bb844661
    Signed-off-by: ktimoney <kevin.timoney@est.tech>

---
+++ policy-db-migrator/src/main/docker/prepare_upgrade.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2021 Nordix Foundation.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
+
+if [ $# -lt 1 ]; then
+    echo "args: schema-name" >&2
+    exit 1
+fi
+
+export SCHEMA=$1
+export POLICY_HOME=/opt/app/policy
+export operation=upgrade
+
+cd $POLICY_HOME
+
+# Create schema directory for upgrade
+mkdir -p $POLICY_HOME/etc/db/migration/${SCHEMA}/sql/
+# Remove any files from previous operations
+rm -rf $POLICY_HOME/etc/db/migration/${SCHEMA}/sql/* 2>/dev/null
+# Copy files to upgrade directories
+cd /home/policy/sql && find . -type f  -not -path '*/downgrade/*' -not -path '*/downgrade'  -print0  \
+   | cpio --null -pud $POLICY_HOME/etc/db/migration/${SCHEMA}/sql/
+
+releases=$(find $POLICY_HOME/etc/db/migration/${SCHEMA}/sql/*/upgrade -type d | sort -u | rev | cut -f2 -d/ | rev)
+for release in $releases
+do
+        echo "Preparing $operation release version: $release"
+done
+echo "Done"
