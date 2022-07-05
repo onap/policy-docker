b0d5b82a7e7baf168030dd0fce5053c624a7e76a
liamfallon <liam.fallon@est.tech>
2022-07-05 18:49:34 +0100

    Update detmVers script for snapshots
    The current script assumes that snapshot versions are always available.
    However the Linux Foundation has now implemented a policy of deleting
    snapshots on no-master branches that are older than a certain age. THis
    breaks our CSITs.
    
    This updated script copes with this by checking if a snapshot is
    avaialable and, if the snapshot is not available, the script falls back
    to the released version.
    
    This script will be needed on other branches besides master as well.
    
    CLAMP TOSCA Control Loop was not in Honolulu and the CLAMP CSITs ran
    separately in the CLAMP repo. We ported the CSITs over to the
    policy/docker repo in Istanbul. Therefore, we need to disable the CLAMP
    CSITs in policy/docker in honolulu
    
    Keystores have been updated to contain up to date certificates.
    
    Issue-ID: POLICY-4233
    Change-Id: I83f0a6816942d5bfaef7c22fc69625f40ae74d23
    Signed-off-by: liamfallon <liam.fallon@est.tech>
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>

--- csit/clamp/plans/setup.sh
+++ csit/clamp/plans/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#
-# ===========LICENSE_START====================================================
-#  Copyright (C) 2020-2021 AT&T Intellectual Property. All rights reserved.
-# ============================================================================
+# ============LICENSE_START=======================================================
+#  Copyright (C) 2022 Nordix Foundation.
+# ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ============LICENSE_END=====================================================
 #
-
-docker logs drools-apps
-docker-compose -f ${SCRIPTS}/docker-compose-all.yml down -v
+# SPDX-License-Identifier: Apache-2.0
+# ============LICENSE_END=========================================================
